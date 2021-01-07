affc5b115b30f3ecb68932c88aa747fb683a8c75
BillyA287 <48728966+BillyA287@users.noreply.github.com>
2021-01-07 22:19:11 +0000

    Updates

---
+++ precache-manifest.63407beeb52d7bbb96610477f4062f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc78611005ae8edc05353875ce5c9c2",
+    "url": "/helpdesk/index.html"
+  },
+  {
+    "revision": "c4b2ea2f7dd567529e9e",
+    "url": "/helpdesk/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f9f1d0ae2512a571a1f",
+    "url": "/helpdesk/static/js/2.f26de220.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/helpdesk/static/js/2.f26de220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b2ea2f7dd567529e9e",
+    "url": "/helpdesk/static/js/main.bf97b82d.chunk.js"
+  },
+  {
+    "revision": "1d3f5f54907e6aa15723",
+    "url": "/helpdesk/static/js/runtime-main.077845f9.js"
+  }
+]);
